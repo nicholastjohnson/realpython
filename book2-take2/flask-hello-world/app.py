99b5a2478262e9301857ab33f1561c166b398c33
Nick Johnson <nick@nicholastjohnson.com>
2014-09-05 14:23:43 -0400

    through page 57

--- book2-take2/flask-hello-world/app.py
+++ book2-take2/flask-hello-world/app.py
@@ -1,22 +1,54 @@
 # ----- Flask Hello World ---- #
 # import the Flask class from the flask module
-
 from flask import Flask
 
-# create the application object
-
+#create the application object
 app = Flask(__name__)
 
-# use decorators to link the function to a url
+# error handling
+app.config["DEBUG"] = True
 
+
+#use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
-# define the view using a function, which returns a string
-
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!?!?!!?"
 
-# start the development server using the run() method
 
+# dynamic routing
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print value + 1
+    return "correct"
+
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print value + 1
+    return "correct"
+
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print value
+    return "correct"
+
+
+# dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "nick":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
+
+
+# start the server
 if __name__ == "__main__":
     app.run()
