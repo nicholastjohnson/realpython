c70e74994ed088dca338a58e6cdac0273d309cc0
Nick Johnson <nick@nicholastjohnson.com>
2014-09-05 14:40:38 -0400

    added sqlite stuff

---
+++ book2-take2/sql/sqla.py
@@ -0,0 +1,18 @@
+#create a sqlite3 database and table
+
+#import the sqlite3 library
+import sqlite3
+
+#create a new dataase if the database doesn't already exist
+conn = sqlite3.connect("new.db")
+
+#get a cursor object used to execute sql commands
+cursor = conn.cursor()
+
+#create a table
+cursor.execute("""CREATE TABLE population
+                (city TEXT, state TEXT, population INT)
+                """)
+
+#close the database connection
+conn.close()
